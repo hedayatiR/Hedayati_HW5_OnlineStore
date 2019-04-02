4b5ff5fcf44d65175cd6e0da1864aa0e0e8f9b68
hedayatiR <gunner_1369@yahoo.com>
2019-04-02 12:42:55 +0430

    add Product class with some fields

---
+++ Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    private int type;
+    private int id;
+    private String name;
+    private int unitPrice;
+    private int numbersAvailable;
+    private String descriptions;
+}
